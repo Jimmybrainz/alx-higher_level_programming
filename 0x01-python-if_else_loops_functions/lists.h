b85fcf362d57bc2e05d40f6ad820b560fad0317a
Jimmybrainz <jim44mike@gmail.com>
2023-11-28 15:50:48 +0000

    function in C that inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -16,7 +17,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
